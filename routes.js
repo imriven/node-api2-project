61a87793f2b6085e4491d56762e3470415eff1cb
Christina Harris <christinathedesigner@outlook.com>
2020-05-05 08:55:45 -0700

    route skeletons done

--- routes.js
+++ routes.js
@@ -1,19 +1,33 @@
-
-const express = require('express');
+const express = require("express");
 
 const router = express.Router(); //creates new router
 
-router.get('/', (req, res) => {
-  res.status(200).send('hello from the GET /posts endpoint');
+router.post("/", (req, res) => {
+  res.status(200).send("hello from the GET /posts endpoint");
 });
 
-router.get('/:id', (req, res) => {
-  res.status(200).send('hello from the GET /users/:id endpoint');
+router.post("/:id/comments", (req, res) => {
+  res.status(200).send("hello from the GET /users/:id endpoint");
 });
 
-router.post('/', (req, res) => {
-  res.status(200).send('hello from the POST /users endpoint');
+router.get("/", (req, res) => {
+  res.status(200).send("hello from the POST /users endpoint");
 });
 
+router.get("/:id", (req, res) => {
+  res.status(200).send("hello from the POST /users endpoint");
+});
 
-module.exports = router; 
+router.get("/:id/comments", (req, res) => {
+  res.status(200).send("hello from the POST /users endpoint");
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).send("hello from the POST /users endpoint");
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).send("hello from the POST /users endpoint");
+});
+
+module.exports = router;
